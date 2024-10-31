b7734bce7c0e3d85bac9c59b97c7f17aa30db002
abraham <wole1702@gmail.com>
2024-10-31 02:01:56 +0100

    adde

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,22 +1,21 @@
 #!/usr/bin/env python3
-"""Create a class LRUCache that inherits from
-BaseCaching and is a caching system:
+"""Create a class MRUCache that inherits
+from BaseCaching and is a caching system:
 
-You must use self.cache_data - dictionary from
-the parent class BaseCaching
+You must use self.cache_data - dictionary
+from the parent class BaseCaching
 You can overload def __init__(self): but don’t
 forget to call the parent init: super().__init__()
 def put(self, key, item):
 Must assign to the dictionary self.cache_data the
 item value for the key key.
-If key or item is None, this method should not do
-anything.
+If key or item is None, this method should not do anything.
 If the number of items in self.cache_data is higher
 that BaseCaching.MAX_ITEMS:
-you must discard the least recently used item
-(LRU algorithm)
-you must print DISCARD: with the key discarded
-and following by a new line
+you must discard the most recently used item
+(MRU algorithm)
+you must print DISCARD: with the key discarded and
+following by a new line
 def get(self, key):
 Must return the value in self.cache_data linked to key.
 If key is None or if the key doesn’t exist in
@@ -27,7 +26,7 @@ self.cache_data, return None.
 BaseCaching = __import__('base_caching').BaseCaching
 
 
-class LRUCache(BaseCaching):
+class MRUCache(BaseCaching):
     """_summary_
     """
 
@@ -52,7 +51,7 @@ class LRUCache(BaseCaching):
                 self.usedKeys.append(
                     self.usedKeys.pop(self.usedKeys.index(key)))
             if len(self.usedKeys) > BaseCaching.MAX_ITEMS:
-                discard = self.usedKeys.pop(0)
+                discard = self.usedKeys.pop(-2)
                 del self.cache_data[discard]
                 print('DISCARD: {:s}'.format(discard))
 
