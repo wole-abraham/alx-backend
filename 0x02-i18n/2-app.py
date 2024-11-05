4932d66157d4c26e9f93b7a7cb0d8ce6ba587204
abraham <wole1702@gmail.com>
2024-11-05 23:34:38 +0100

    fixe

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,3 +1,30 @@
-#!/usr/bin/python3
-""" Python scripts """
-from flask import Flask
+#!/usr/bin/env python3
+""" Flask babel setup """
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+
+
+class Config:
+    """ Configuration for babel  """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """ get best locale """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route("/")
+def index():
+    """ index page """
+    return render_template("2-index.html")
