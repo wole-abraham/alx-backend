75a0ac37f37f63432b58d1736a48513df0b3042b
abraham <wole1702@gmail.com>
2024-11-05 22:06:07 +0100

    fixes

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 """ Flask app """
 from flask import Flask, render_template
-
+from typing import Text
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def index():
+def index() -> Text:
     """ index page """
     return render_template("index.html")
